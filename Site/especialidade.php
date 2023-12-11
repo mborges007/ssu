a2365e49dd6a34459eeee8c2d6ef57ee540082ad
Alvarez-T <thiago.alvarez2012@gmail.com>
2023-12-11 13:18:06 -0300

    Correção

--- Site/especialidade.php
+++ Site/especialidade.php
@@ -4,18 +4,9 @@ class Especialidade {
     private int $id;
     private string $descricao;
 
-<<<<<<< Updated upstream
     public function __construct() {
         
     }
-=======
-<<<<<<< HEAD
-=======
-    public function __construct() {
-        
-    }
->>>>>>> 48a5a95b014a2b56cfc8e28ed88c8027bbddca61
->>>>>>> Stashed changes
     public function getId(): int {
         return $this->id;
     }
--- Site/medico.php
+++ Site/medico.php
@@ -1,21 +1,5 @@
 <?php
 
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-require_once('database.php');
-
-class Medico {
-    private int $id;
-    private string $nome;
-    private string $crm;
-    private int $especialidade;
-
-
-    public function __construct() {
-        $this->db = new Database();
-=======
->>>>>>> Stashed changes
 class Medico {
     private int $id;
     private string $nome = "";
@@ -26,10 +10,6 @@ class Medico {
 
     public function __construct() {
         
-<<<<<<< Updated upstream
-=======
->>>>>>> 48a5a95b014a2b56cfc8e28ed88c8027bbddca61
->>>>>>> Stashed changes
     }
 
     public function getId(): int {
@@ -56,16 +36,6 @@ class Medico {
         $this->crm = $crm;
     }
 
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-    public function getEspecialidade(): int {
-        return $this->especialidade;
-    }
-
-    public function setEspecialidade(int $especialidade){
-=======
->>>>>>> Stashed changes
     public function getCidade(): string {
         return $this->cidade;
     }
@@ -79,10 +49,6 @@ class Medico {
     }
 
     public function setEspecialidade(Especialidade $especialidade){
-<<<<<<< Updated upstream
-=======
->>>>>>> 48a5a95b014a2b56cfc8e28ed88c8027bbddca61
->>>>>>> Stashed changes
         $this->especialidade = $especialidade;
     }
 }
